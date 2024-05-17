76a986f67054e97e2de4dc1ccb0eb978efa4c061
Kamil Prorok <kamilxprorok@gmail.com>
2024-05-17 22:47:02 +0200

    Added script for downloading page status

---
+++ src/includes/get_service_status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+URL=$1
+
+# Check if URL is provided
+if [ -z "$URL" ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+DATA=$(curl -o /dev/null -s -w "%{http_code}:%{time_total}" $URL)
+echo $DATA
